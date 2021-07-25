c96e3b42fbcb2e338575a4e1b72e1916f55f3e63
Julia Joseph <juljoe726@gmail.com>
2021-07-25 17:02:42 +0530

    adding programmatic navigation

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,24 @@
+import { useRouter } from "next/router";
+
 const ClientsProjectPage = () => {
+    const router = useRouter();
+
+    console.log(router.query);
+
+    const projectHandler = () => {
+        router.push({
+            pathname: '/clients/[id]/[clientprojectsid]',
+            query: {
+                id: 'jeremy',
+                clientprojectsid: 'projecta'
+            }
+        })
+    }
+
     return (
         <div>
-            List of projects of a given Client
+            <h1>List of projects of a given Client</h1>
+            <button onClick={projectHandler}> Load Project A </button>
         </div>
     )
 }
