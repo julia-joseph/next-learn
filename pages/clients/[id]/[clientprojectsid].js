c0510789ca3cddd6f6646d579c7a625853aac627
Julia Joseph <juljoe726@gmail.com>
2021-07-25 16:19:05 +0530

    adding useRouter to multinesting page

--- pages/clients/[id]/[clientprojectsid].js
+++ pages/clients/[id]/[clientprojectsid].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 const SelectedClientProjectPage = () => {
+    const router = useRouter();
+
+    console.log("router query", router.query);
+
     return (
         <div>
             The selected project of a given Client
