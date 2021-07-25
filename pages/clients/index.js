6008f79682482effcea29b2d7b45ad62626ef825
Julia Joseph <juljoe726@gmail.com>
2021-07-25 16:41:41 +0530

    clients page - dynamic route navigation

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,27 @@
+import Link from "next/link"
+
 const ClientsPage = () => {
+    const clients = [
+        {
+            id: 'jeremy',
+            name: 'Jeremy Butch'
+        },
+        {
+            id: 'cindy',
+            name: 'Cindy Hoffman'
+        },
+    ];
+
     return (
         <div>
-            Clients Page
+            <h1>Clients Page</h1>
+            <ul>
+                {clients.map(client => (
+                    <li key={client.id}>
+                        <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
