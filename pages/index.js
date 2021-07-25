c0d08c5ebae3381ee283baeaa5e0486f1f74ea16
Julia Joseph <juljoe726@gmail.com>
2021-07-25 16:34:39 +0530

    adding navigation with Link component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 const HomePage = () => {
     return (
         <div>
-            Home Page
+            <h1>Home Page</h1>
+            <ul>
+                <li>
+                    <Link href="/portfolio">Portfolio Page</Link>
+                </li>
+                <li>
+                    <Link href="/clients">Clients Page</Link>
+                </li>
+            </ul>
         </div>
     )
 }
